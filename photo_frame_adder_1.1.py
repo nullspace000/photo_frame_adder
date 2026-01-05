d1d5fb229788e49ea982a0e1268ee6d6a99d99e9
nullspace <nullspace000@protonmail.com>
2026-01-05 00:10:32 -0600

    a

--- photo_frame_adder_1.1.py
+++ photo_frame_adder_1.1.py
@@ -8,7 +8,7 @@ width, height = img.size
 # Determine the longer axis
 longer_axis = max(width, height)
 
-# 10% of the longer axis (must be an int)
+# 5% of the longer axis (must be an int)
 border_size = int(longer_axis * 0.05)
 
 # Add white frame
